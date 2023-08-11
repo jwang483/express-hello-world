53dcdde17bb3252ed784009598657acf2b867646
jiajie wang <55546624+jwang483@users.noreply.github.com>
2023-08-11 16:53:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
 
-const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
